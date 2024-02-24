ff58b51fcc02ece5262b6cecaa710c481efae119
petemaynard <xerxespete@gmail.com>
2024-02-24 00:26:04 -0600

    set up models

---
+++ models/Product.js
@@ -0,0 +1,49 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model { }
+
+// set up fields and rules for Product model
+Product.init(
+   {
+      prod_id: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         primaryKey: true,
+         autoIncrement: true
+      },
+      product_name: {
+         type: DataTypes.STRING,
+         allowNull: false,
+      },
+      price: {
+         type: DataTypes.DECIMAL(4, 2),
+         // Wants me to validate decimal???
+      },
+      stock: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         // default value of 10???
+         // Validate numeric???
+      },
+      category_id: {
+         type: DataTypes.INTEGER,
+         references: {
+            model: 'category',
+            key: 'cat_id'
+         }
+      }
+   },
+   {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'product',
+   }
+);
+
+module.exports = Product;
---
+++ models/ProductTag.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model { }
+
+ProductTag.init(
+   {
+      prodtag_id: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         primaryKey: true,
+         autoIncrement: true
+      },
+      product_id: {
+         type: DataTypes.INTEGER,
+         references: {
+            model: 'product',
+            key: 'prod_id'
+         }
+      },
+      tag_id: {
+         type: DataTypes.INTEGER,
+         references: {
+            model: 'tag',
+            key: 'tag_id'
+         }
+      }
+   },
+   {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'product_tag',
+   }
+);
+
+module.exports = ProductTag;
